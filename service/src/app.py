1fd75af7ed213d8fa6f5dd113be4562848fa0ca1
Ben Medcalf <ben.medcalf@gmail.com>
2025-06-18 15:18:27 -0400

    and update app.py to do all that stuff

--- service/src/app.py
+++ service/src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.neo4j_client import driver, fetch_schema
+from src.neo4j_client import driver, execute_query, fetch_schema
+from src.requests import QueryAuraDBRequest
 
 app = FastAPI()
 
@@ -13,12 +14,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/api/schema")
 async def get_schema():
     schema = fetch_schema(driver)
     return schema
 
+@app.post("/api/query")
+async def query(request: QueryAuraDBRequest):
+    result = execute_query(driver, request.cypher_query)
+    return result
 
 @app.get("/api/healthcheck")
 async def healthcheck():
