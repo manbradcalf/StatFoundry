3d62e969ac4de6524f2c5f18eb5c9f93aed36d33
Ben Medcalf <ben.medcalf@gmail.com>
2025-06-21 10:17:25 -0400

    better, dynamic handrolled chunks-data

--- ui/src/chunks-data.ts
+++ ui/src/chunks-data.ts
@@ -65,8 +65,159 @@ export function getAvailableChunks(): Chunk[] {
       ],
       Slots: [],
     },
+
+    // Generic Filter Chunks
     {
-      // FILTER
+      English: "where player {property} {operator} {value}",
+      Cypher: "WHERE p.{property} {operator} $value",
+      QueryType: QueryType.FILTER,
+      Inputs: [["p", DataType.Player]],
+      Outputs: [["p", DataType.Player]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "position",
+        },
+        {
+          Name: "operator",
+          Value: "=",
+        },
+        {
+          Name: "value",
+          Value: "QB",
+        },
+      ],
+    },
+    {
+      English: "where player game {property} {operator} {value}",
+      Cypher: "WHERE pg.{property} {operator} $value",
+      QueryType: QueryType.FILTER,
+      Inputs: [["pg", DataType.PlayerGame]],
+      Outputs: [["pg", DataType.PlayerGame]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "season",
+        },
+        {
+          Name: "operator",
+          Value: "=",
+        },
+        {
+          Name: "value",
+          Value: 2024,
+        },
+      ],
+    },
+    {
+      English: "where team {property} {operator} {value}",
+      Cypher: "WHERE t.{property} {operator} $value",
+      QueryType: QueryType.FILTER,
+      Inputs: [["t", DataType.Team]],
+      Outputs: [["t", DataType.Team]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "name",
+        },
+        {
+          Name: "operator",
+          Value: "=",
+        },
+        {
+          Name: "value",
+          Value: "Bills",
+        },
+      ],
+    },
+    {
+      English: "where player {property} is between {min} and {max}",
+      Cypher: "WHERE p.{property} >= $min AND p.{property} <= $max",
+      QueryType: QueryType.FILTER,
+      Inputs: [["p", DataType.Player]],
+      Outputs: [["p", DataType.Player]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "weight",
+        },
+        {
+          Name: "min",
+          Value: 200,
+        },
+        {
+          Name: "max",
+          Value: 250,
+        },
+      ],
+    },
+    {
+      English: "where player game {property} is between {min} and {max}",
+      Cypher: "WHERE pg.{property} >= $min AND pg.{property} <= $max",
+      QueryType: QueryType.FILTER,
+      Inputs: [["pg", DataType.PlayerGame]],
+      Outputs: [["pg", DataType.PlayerGame]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "season",
+        },
+        {
+          Name: "min",
+          Value: 2023,
+        },
+        {
+          Name: "max",
+          Value: 2024,
+        },
+      ],
+    },
+    {
+      English: "where player {property} contains {value}",
+      Cypher: "WHERE p.{property} CONTAINS $value",
+      QueryType: QueryType.FILTER,
+      Inputs: [["p", DataType.Player]],
+      Outputs: [["p", DataType.Player]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "display_name",
+        },
+        {
+          Name: "value",
+          Value: "Allen",
+        },
+      ],
+    },
+    {
+      English: "where player {property} is null",
+      Cypher: "WHERE p.{property} IS NULL",
+      QueryType: QueryType.FILTER,
+      Inputs: [["p", DataType.Player]],
+      Outputs: [["p", DataType.Player]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "team_abbr",
+        },
+      ],
+    },
+    {
+      English: "where player {property} is not null",
+      Cypher: "WHERE p.{property} IS NOT NULL",
+      QueryType: QueryType.FILTER,
+      Inputs: [["p", DataType.Player]],
+      Outputs: [["p", DataType.Player]],
+      Slots: [
+        {
+          Name: "property",
+          Value: "team_abbr",
+        },
+      ],
+    },
+
+    // Legacy specific filters (keeping for backward compatibility)
+    {
       English: "who play {position}",
       Cypher: "WHERE p.position = $position",
       QueryType: QueryType.FILTER,
@@ -113,6 +264,7 @@ export function getAvailableChunks(): Chunk[] {
         },
       ],
     },
+
     // RETURN
     {
       English: "return player names",
