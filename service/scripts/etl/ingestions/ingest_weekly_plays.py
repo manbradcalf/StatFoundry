21a755e920f6530d36230792231261cc38ba5151
Ben Medcalf <ben.medcalf@gmail.com>
2025-09-04 15:55:43 -0400

    add todos to wip ingestion

--- service/scripts/etl/ingestions/ingest_weekly_plays.py
+++ service/scripts/etl/ingestions/ingest_weekly_plays.py
@@ -1,6 +1,18 @@
 from src.neo4j_client import driver
 
-print("ingest_weekly_plays called")
-# TODO: Update
-query = "MERGE (n:TestNode {name:'This is a test node'}) RETURN (n) LIMIT 1"
+print("ingest_weekly_pbp called")
+# TODO:
+print("Downloading play_by_play_2025.csv")
+# TODO:
+print("Creating id column from game_id + play_id")
+# TODO:
+print("Transforming CSV Data for Cypher script (ints to bools, etc)")
+# TODO:
+print("Running Cypher ingestion script")
+
+
+query = """
+    MATCH (n) RETURN (n) RETURN n LIMIT 1
+    """
+
 driver.execute_query(query)
