99e87a713baf15f85fc4f199c283e86054266979
Ben Medcalf <ben.medcalf@gmail.com>
2025-10-05 23:04:07 -0400

    team_name to name for team

--- ui/src/feature/Chunks/Views/TeamLabelView.ts
+++ ui/src/feature/Chunks/Views/TeamLabelView.ts
@@ -1,4 +1,6 @@
-export const TEAM_LABEL_PROPERTIES = [  { key: "team_abbr", type: "String" },];
+export const TEAM_LABEL_PROPERTIES = [  { key: "name", type: "String" },
+  { key: "team_abbr", type: "String" },];
 export interface TeamProperties {
+  name: string;
   team_abbr: string;
 }
